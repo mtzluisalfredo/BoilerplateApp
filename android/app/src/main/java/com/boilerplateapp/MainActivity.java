988367ee1d462478f9cb3ef3d25328abfefab7ba
LuisAlfredoMtz <luisalfredomtz@hotmail.com>
2019-02-09 23:25:15 -0600

    style:code

--- android/app/src/main/java/com/boilerplateapp/MainActivity.java
+++ android/app/src/main/java/com/boilerplateapp/MainActivity.java
@@ -1,9 +1,7 @@
 package com.boilerplateapp;
 
-// import com.facebook.react.ReactActivity;
 import com.reactnativenavigation.NavigationActivity;
 
-// public class MainActivity extends ReactActivity {
 public class MainActivity extends NavigationActivity {
 
     /**
