d287496c24d1faf27ccdc86cc5e571038693acf8
LuisAlfredoMtz <luisalfredomtz@hotmail.com>
2019-02-09 21:12:24 -0600

    navigation- index

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-/**
- * @format
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
+import { Navigation } from "react-native-navigation";
+import App from "./App";
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App);
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: "navigation.playground.WelcomeScreen"
+      }
+    }
+  });
+});
+
+// /**
+//  * @format
+//  * @lint-ignore-every XPLATJSCOPYRIGHT1
+//  */
+
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
+
+// AppRegistry.registerComponent(appName, () => App);
+
